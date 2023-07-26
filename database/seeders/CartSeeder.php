945d780bfe34fe68ddb086894daeec0988e83074
TrieuHongKy <trieuhongky11112003@gmail.com>
2023-07-27 00:43:33 +0700

    Add Cart Seeder And Receipt Seeder

--- database/seeders/CartSeeder.php
+++ database/seeders/CartSeeder.php
@@ -6,20 +6,19 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ReceiptSeeder extends Seeder
+class CartSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('receipts')->insert([
+        DB::table('carts')->insert([
             [
-                'cart_id'=>'1',
-                'date'=>'2023-7-26',
-                'payment_method'=>'Ship COD',
-                'address'=>'Can Tho',
-                'total'=>'1700000',
+                'product_id'=>'1',
+                'user_id'=>'1',
+                'total'=>'700000',
+                'quantity'=>'4',
                 'created_at'=>Now(),'updated_at'=>Now()
             ],
         ]);
